12559309c2e25118f3a3b844d4b2380e801aa8db
kodai3 <k3dai.su3@gmail.com>
2022-02-18 21:25:42 +0900

    feat: 🎸 expose decorators

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-if (module && module.hot && module.hot.decline) {
-  module.hot.decline();
-}
-
-export const withLaunchDarkly = () => (): JSX.Element => {
-  throw new Error(
-    'Please look at the new configuration for storybook-addon-launchdarkly'
-  );
-};
+export { decorators } from './preset/addDecorator';
