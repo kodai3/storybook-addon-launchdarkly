4d1a59142c12dfe7c14445955157ac892fff1652
kodai3 <k3dai.su3@gmail.com>
2023-07-11 23:47:37 +0900

    feat: 🎸 ld client v3

--- src/title.ts
+++ src/title.ts
@@ -1,10 +1,10 @@
 import { useParameter } from '@storybook/api';
-import { LDContext } from 'launchdarkly-react-client-sdk/lib/context';
+import type { ReactSdkContext } from 'launchdarkly-react-client-sdk/lib/context';
 import { PARAM_KEY } from './constants';
 
 export function getTitle(): string {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const params = useParameter<LDContext>(PARAM_KEY);
+  const params = useParameter<ReactSdkContext>(PARAM_KEY);
 
   if (params?.flags) {
     return `Launchdarkly (${Object.keys(params.flags).length})`;
